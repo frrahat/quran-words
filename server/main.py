4cd3430a040cc0a9a220e980e24f38c19198baa1
frrahat <fr.rahat@gmail.com>
2021-02-28 02:47:00 +0600

    list verses, add http and consider query for pagination urls

--- server/main.py
+++ server/main.py
@@ -3,13 +3,13 @@ from typing import List, Optional
 from fastapi import Depends, FastAPI, Query
 from pydantic import BaseModel
 
-from .db import db, Level, Word
+from .db import db, Level, Word, Verse
 
 
 app = FastAPI()
 
 
-BASE_URL = 'localhost:8000'
+BASE_URL = 'http://localhost:8000'
 
 
 class PaginationResponseModel(BaseModel):
@@ -41,11 +41,25 @@ class WordListResponseModel(BaseModel):
     pagination: PaginationResponseModel
 
 
+class VerseResponseModel(BaseModel):
+    sura: int
+    ayah: int
+    arabic: str
+    english: str
+
+
+class VerseListResponseModel(BaseModel):
+    data: List[VerseResponseModel]
+    total: int
+    pagination: PaginationResponseModel
+
+
 def get_pagination_response(
                 url: str,
                 count: int,
                 current_offset: int,
                 current_pagesize: int,
+                additional_query_string: Optional[str] = None,
                 limit: int = 10) -> dict[str, Optional[str]]:
     
     prev_offset = max(0, current_offset - limit)
@@ -53,11 +67,13 @@ def get_pagination_response(
 
     next_offset = current_offset + current_pagesize
     next_pagesize = min(next_offset + limit, count - next_offset + 1)
+    
+    additional_query_string = f'&{additional_query_string}' if additional_query_string else ''
 
     return {
-        'previous': f'{url}?offset={prev_offset}&pagesize={prev_pagesize}' \
+        'previous': f'{url}?offset={prev_offset}&pagesize={prev_pagesize}{additional_query_string}' \
             if current_offset > 0 else None,
-        'next': f'{url}?offset={next_offset}&pagesize={next_pagesize}' \
+        'next': f'{url}?offset={next_offset}&pagesize={next_pagesize}{additional_query_string}' \
             if next_offset < count else None,
     }
 
@@ -110,6 +126,8 @@ def list_word(level: Optional[int] = Query(None, gt=0), pagination_parameters: d
 
     words = base_query.order_by(Word.level_num, Word.serial_num).offset(offset).limit(pagesize).all()
 
+    additional_query_string = f'level={level}' if level else ''
+
     return {
         'data': [
             {
@@ -120,5 +138,41 @@ def list_word(level: Optional[int] = Query(None, gt=0), pagination_parameters: d
             } for word in words
         ],
         'total': total,
-        'pagination': get_pagination_response(f'{BASE_URL}/words', total, offset, pagesize)
+        'pagination': get_pagination_response(
+            f'{BASE_URL}/words', total, offset, pagesize, additional_query_string)
+    }
+
+
+@app.get('/verses', response_model=VerseListResponseModel)
+def list_verse(sura: Optional[int] = Query(None, gt=0, le=114),
+               ayah: Optional[int] = Query(None, gt=0, le=286),
+               pagination_parameters: dict = Depends(pagination_parameters)):
+    offset = pagination_parameters['offset']
+    pagesize = pagination_parameters['pagesize']
+
+    base_query = db.session.query(Verse)
+
+    if sura:
+        base_query = base_query.filter(Verse.sura_num == sura)
+    
+    if ayah:
+        base_query = base_query.filter(Verse.ayah_num == ayah)
+
+    total = base_query.count()
+
+    verses = base_query.order_by(Verse.sura_num, Verse.ayah_num).offset(offset).limit(pagesize).all()
+
+    additional_query_string = '&'.join(f'{k}={v}' for k, v in {'sura': sura, 'ayah': ayah}.items() if v)
+    return {
+        'data': [
+            {
+                'sura': verse.sura_num,
+                'ayah': verse.ayah_num,
+                'arabic': verse.text,
+                'english': '<Not available>',
+            } for verse in verses
+        ],
+        'total': total,
+        'pagination': get_pagination_response(
+            f'{BASE_URL}/verses', total, offset, pagesize, additional_query_string)
     }
