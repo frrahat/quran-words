3ede843a50f0fd9331984507719db4b3bf6f50b3
frrahat <fr.rahat@gmail.com>
2021-04-14 16:48:24 +0600

    add tests for pagination utils

--- server/utils.py
+++ server/utils.py
@@ -4,6 +4,28 @@ import urllib.parse
 from fastapi import Request
 
 
+def get_next_prev_paginations(
+        current_offset: int,
+        current_pagesize: int,
+        count: int):
+
+    prev_pagination = {}
+    next_pagination = {}
+
+    if current_offset > 0:
+        prev_pagination['offset'] = max(0, current_offset - current_pagesize)
+        prev_pagination['pagesize'] = min(current_pagesize, current_offset)
+
+    next_offset = current_offset + current_pagesize
+
+    if next_offset < count:
+        next_pagination['offset'] = next_offset
+        next_pagination['pagesize'] = min(
+            current_pagesize, count - next_offset + 1)
+
+    return prev_pagination, next_pagination
+
+
 def get_pagination_response(
         request: Request,
         count: int,
@@ -13,19 +35,16 @@ def get_pagination_response(
     current_offset = int(request.query_params.get('offset', 0))
     current_pagesize = int(request.query_params.get('pagesize', limit))
 
-    prev_offset = max(0, current_offset - current_pagesize)
-    prev_pagesize = min(current_pagesize, current_offset)
-
-    next_offset = current_offset + current_pagesize
-    next_pagesize = min(current_pagesize, count - next_offset + 1)
+    prev_pagination, next_pagination = get_next_prev_paginations(
+        current_offset, current_pagesize, count)
 
     additional_query_string = f'&{additional_query_string}' if additional_query_string else ''
 
     url = urllib.parse.urljoin(str(request.base_url), request.url.path)
 
     return {
-        'previous': f'{url}?offset={prev_offset}&pagesize={prev_pagesize}{additional_query_string}'
-        if current_offset > 0 else None,
-        'next': f'{url}?offset={next_offset}&pagesize={next_pagesize}{additional_query_string}'
-        if next_offset < count else None,
+        'previous': f"{url}?offset={prev_pagination['offset']}&pagesize={prev_pagination['pagesize']}{additional_query_string}"
+        if prev_pagination else None,
+        'next': f"{url}?offset={next_pagination['offset']}&pagesize={next_pagination['pagesize']}{additional_query_string}"
+        if next_pagination else None,
     }
