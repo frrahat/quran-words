aee6c0c26b14bedea2d175bbd2c6c0b5b57df2e9
frrahat <fr.rahat@gmail.com>
2021-05-02 22:30:24 +0600

    update Occurrences component and convert that to tsx

--- client/src/components/Occurrences.tsx
+++ client/src/components/Occurrences.tsx
@@ -8,26 +8,36 @@ import loaderGif from "../images/loader.gif";
 
 import './Occurrences.css';
 
-function VerseLabel({ suraNum, ayahNum, wordIndex }) {
+function VerseLabel({ suraNum, ayahNum, wordIndex }: {
+  suraNum: number,
+  ayahNum: number,
+  wordIndex: number,
+}) {
   return (
     <Link
       className="Occurrences-VerseLabel"
       to={gerneratePageLink(suraNum, ayahNum, wordIndex, true)}
-      >
+    >
       {suraNum}:{ayahNum}
     </Link>
   )
 }
 
-function VerseWord({ word, isHighlighted }) {
+function VerseWord({ word, isHighlighted }: {
+  word: string,
+  isHighlighted: boolean,
+}) {
   return (
-    <span className={`Occurrences-VerseWord${isHighlighted ? '-highlighted': ''}`}>
+    <span className={`Occurrences-VerseWord${isHighlighted ? '-highlighted' : ''}`}>
       {word}
     </span>
   )
 }
 
-function Verse({ verse, wordIndex }) {
+function Verse({ verse, wordIndex }: {
+  verse: string,
+  wordIndex: number,
+}) {
   return (
     <div className="Occurrences-Verse">
       {verse.split(' ').map((word, index) =>
@@ -41,23 +51,30 @@ function Verse({ verse, wordIndex }) {
   )
 }
 
-function OccurrencesItem({ suraNum, ayahNum, wordIndex, verse}) {
+function OccurrencesItem({ suraNum, ayahNum, wordNums, verse }: {
+  suraNum: number,
+  ayahNum: number,
+  wordNums: number[],
+  verse: string,
+}) {
   return (
     <div className="Occurrences-Item">
       <VerseLabel
         suraNum={suraNum}
         ayahNum={ayahNum}
-        wordIndex={wordIndex}
+        wordIndex={wordNums[0]}
       />
       <Verse
         verse={verse}
-        wordIndex={wordIndex}
+        wordIndex={wordNums[0]}
       />
     </div>
   )
 }
 
-function Occurrences({ word_root }) {
+function Occurrences({ word_root }: {
+  word_root: string,
+}) {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
 
@@ -87,15 +104,20 @@ function Occurrences({ word_root }) {
       {
         isLoading ?
           <img src={loaderGif} alt="loader" />
-          : data.map(({sura, ayah, word_num, verse}, index) => (
-          <OccurrencesItem
-            key={`Occurrences-${index}`}
-            suraNum={sura}
-            ayahNum={ayah}
-            wordIndex={word_num - 1}
-            verse={verse}
-          />
-        ))
+          : data.map(({
+            sura,
+            ayah,
+            word_nums,
+            verse: { arabic, english, words }
+          }, index) => (
+            <OccurrencesItem
+              key={`Occurrences-${index}`}
+              suraNum={sura}
+              ayahNum={ayah}
+              wordNums={word_nums}
+              verse={arabic}
+            />
+          ))
       }
     </div>
   )
