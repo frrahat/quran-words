9dd58f79d02ee8914e6c6f489e3be3d5e65f06ce
frrahat <fr.rahat@gmail.com>
2023-03-26 16:41:08 +0600

    add module to get taraweeh ayat

---
+++ server/tests/test_taraweeh_ayat.py
@@ -0,0 +1,10 @@
+from server.taraweeh_ayat import get_start_end_ayah_by_day, AyahInfo
+
+
+def test_get_start_end_ayah_by_day_returns_properly():
+    assert get_start_end_ayah_by_day(2) == (AyahInfo(2, 204), AyahInfo(3, 91))
+
+
+def test_get_start_end_ayah_by_day_does_not_break():
+    for day in range(-30, 31):
+        get_start_end_ayah_by_day(day)
