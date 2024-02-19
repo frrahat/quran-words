c62202256662877314731c84c04b3dff1c6f64e6
frrahat <fr.rahat@gmail.com>
2024-02-19 12:15:10 +0600

    format db_corpus.py

--- server/db_corpus.py
+++ server/db_corpus.py
@@ -4,8 +4,9 @@ from .config import CONFIG
 
 
 db_corpus = SQLAlchemy(
-    uri='sqlite:///server/databases/corpus.db?check_same_thread=False',
-    echo=CONFIG.ECHO_SQL)
+    uri="sqlite:///server/databases/corpus.db?check_same_thread=False",
+    echo=CONFIG.ECHO_SQL,
+)
 
 
 POS_FULL_FORMS_AND_COLORS = {
@@ -209,73 +210,75 @@ POS_FULL_FORMS_AND_COLORS = {
 
 
 class VerbForms(db_corpus.Model):  # type: ignore
-    __tablename__ = 'verbs_with_six_forms'
-    root = db_corpus.Column('root', db_corpus.Unicode, primary_key=True)
+    __tablename__ = "verbs_with_six_forms"
+    root = db_corpus.Column("root", db_corpus.Unicode, primary_key=True)
     verb_type = db_corpus.Column(
-        'verb_type', db_corpus.String(length=3), primary_key=True)
-    perfect = db_corpus.Column('perfect', db_corpus.Unicode)
-    imperfect = db_corpus.Column('imperfect', db_corpus.Unicode)
-    imperative = db_corpus.Column('imperative', db_corpus.Unicode)
-    active_participle = db_corpus.Column(
-        'active_participle', db_corpus.Unicode)
-    passive_participle = db_corpus.Column(
-        'passive_participle', db_corpus.Unicode)
-    verbal_noun = db_corpus.Column('verbal_noun', db_corpus.Unicode)
+        "verb_type", db_corpus.String(length=3), primary_key=True
+    )
+    perfect = db_corpus.Column("perfect", db_corpus.Unicode)
+    imperfect = db_corpus.Column("imperfect", db_corpus.Unicode)
+    imperative = db_corpus.Column("imperative", db_corpus.Unicode)
+    active_participle = db_corpus.Column("active_participle", db_corpus.Unicode)
+    passive_participle = db_corpus.Column("passive_participle", db_corpus.Unicode)
+    verbal_noun = db_corpus.Column("verbal_noun", db_corpus.Unicode)
 
     def to_dict(self):
         return {
-            'root': self.root,
-            'verb_type': self.verb_type,
-            'perfect': self.perfect,
-            'imperative': self.imperative,
-            'active_participle': self.active_participle,
-            'passive_participle': self.passive_participle,
-            'verbal_noun': self.verbal_noun,
+            "root": self.root,
+            "verb_type": self.verb_type,
+            "perfect": self.perfect,
+            "imperative": self.imperative,
+            "active_participle": self.active_participle,
+            "passive_participle": self.passive_participle,
+            "verbal_noun": self.verbal_noun,
         }
 
 
 class Corpus(db_corpus.Model):  # type: ignore
-    __tablename__ = 'corpus'
-    sura_num = db_corpus.Column('surah', db_corpus.Integer, primary_key=True)
-    ayah_num = db_corpus.Column('ayah', db_corpus.Integer, primary_key=True)
-    word_num = db_corpus.Column('word', db_corpus.Integer, primary_key=True)
-    count = db_corpus.Column('count', db_corpus.Integer)
-    ar1 = db_corpus.Column('ar1', db_corpus.Unicode)
-    ar2 = db_corpus.Column('ar2', db_corpus.Unicode)
-    ar3 = db_corpus.Column('ar3', db_corpus.Unicode)
-    ar4 = db_corpus.Column('ar4', db_corpus.Unicode)
-    ar5 = db_corpus.Column('ar5', db_corpus.Unicode)
-    pos1 = db_corpus.Column('pos1', db_corpus.Unicode)
-    pos2 = db_corpus.Column('pos2', db_corpus.Unicode)
-    pos3 = db_corpus.Column('pos3', db_corpus.Unicode)
-    pos4 = db_corpus.Column('pos4', db_corpus.Unicode)
-    pos5 = db_corpus.Column('pos5', db_corpus.Unicode)
-    root = db_corpus.Column('root_ar', db_corpus.Unicode)
-    lemma = db_corpus.Column('lemma', db_corpus.Unicode)
-    verb_type = db_corpus.Column('verb_type', db_corpus.String(length=3))
-    verb_form = db_corpus.Column('verf_form', db_corpus.Integer)
-    verb_forms = db_corpus.relationship(
-        'VerbForms', uselist=False, lazy='joined')
+    __tablename__ = "corpus"
+    sura_num = db_corpus.Column("surah", db_corpus.Integer, primary_key=True)
+    ayah_num = db_corpus.Column("ayah", db_corpus.Integer, primary_key=True)
+    word_num = db_corpus.Column("word", db_corpus.Integer, primary_key=True)
+    count = db_corpus.Column("count", db_corpus.Integer)
+    ar1 = db_corpus.Column("ar1", db_corpus.Unicode)
+    ar2 = db_corpus.Column("ar2", db_corpus.Unicode)
+    ar3 = db_corpus.Column("ar3", db_corpus.Unicode)
+    ar4 = db_corpus.Column("ar4", db_corpus.Unicode)
+    ar5 = db_corpus.Column("ar5", db_corpus.Unicode)
+    pos1 = db_corpus.Column("pos1", db_corpus.Unicode)
+    pos2 = db_corpus.Column("pos2", db_corpus.Unicode)
+    pos3 = db_corpus.Column("pos3", db_corpus.Unicode)
+    pos4 = db_corpus.Column("pos4", db_corpus.Unicode)
+    pos5 = db_corpus.Column("pos5", db_corpus.Unicode)
+    root = db_corpus.Column("root_ar", db_corpus.Unicode)
+    lemma = db_corpus.Column("lemma", db_corpus.Unicode)
+    verb_type = db_corpus.Column("verb_type", db_corpus.String(length=3))
+    verb_form = db_corpus.Column("verf_form", db_corpus.Integer)
+    verb_forms = db_corpus.relationship("VerbForms", uselist=False, lazy="joined")
 
-    db_corpus.ForeignKeyConstraint([root, verb_type],
-                                   [VerbForms.root, VerbForms.verb_type])
+    db_corpus.ForeignKeyConstraint(
+        [root, verb_type], [VerbForms.root, VerbForms.verb_type]
+    )
 
     def get_segments(self):
         segments = []
 
         for position in range(1, self.count + 1):
-            arabic = getattr(self, f'ar{position}')
-            pos = getattr(self, f'pos{position}')
+            arabic = getattr(self, f"ar{position}")
+            pos = getattr(self, f"pos{position}")
 
             pos_full_form_and_color = POS_FULL_FORMS_AND_COLORS.get(
-                pos, {'full': None, 'color': None})
+                pos, {"full": None, "color": None}
+            )
 
-            segments.append({
-                'arabic': arabic,
-                'pos': pos,
-                'pos_full': pos_full_form_and_color['full'],
-                'pos_color': pos_full_form_and_color['color'],
-            })
+            segments.append(
+                {
+                    "arabic": arabic,
+                    "pos": pos,
+                    "pos_full": pos_full_form_and_color["full"],
+                    "pos_color": pos_full_form_and_color["color"],
+                }
+            )
 
         return segments
 
@@ -291,15 +294,15 @@ class Corpus(db_corpus.Model):  # type: ignore
         # }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(db_corpus.session.query(Corpus).count())
     print(db_corpus.session.query(VerbForms).count())
 
-    base_query = db_corpus.session.query(Corpus)\
-        .filter(
-            Corpus.sura_num == 1,
-            Corpus.ayah_num == 1,
-            Corpus.word_num == 3).first()
+    base_query = (
+        db_corpus.session.query(Corpus)
+        .filter(Corpus.sura_num == 1, Corpus.ayah_num == 1, Corpus.word_num == 3)
+        .first()
+    )
 
     print(base_query.to_dict())
     print(base_query.verb_forms.to_dict())
