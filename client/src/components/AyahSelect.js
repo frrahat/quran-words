68ff3bc72e2dec4bd1901cd324f5ebf811eb38e0
frrahat <fr.rahat@gmail.com>
2024-02-21 01:46:59 +0600

    refactor NumberSelect from AyahSelect

--- client/src/components/AyahSelect.js
+++ client/src/components/AyahSelect.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import { suraList } from "../config";
+import NumberSelect from "./NumberSelect";
 
 import "./AyahSelect.css";
 
@@ -9,33 +9,16 @@ function AyahSelect({
   selectedAyahNum,
   onSelectAyah,
 }) {
-  const [isOptionsExpanded, setIsOptionsExpanded] = useState(false);
+  const ayahCount = suraList[selectedSuraNum - 1]?.ayah_count || 0;
 
   return (
-    <div
-      className="AyahSelect"
-      onClick={() => setIsOptionsExpanded(!isOptionsExpanded)}
-    >
-      <div className={valueClassName}>{selectedAyahNum}</div>
-      <div
-        className={`AyahSelect-Options${isOptionsExpanded ? "" : "-hidden"}`}
-      >
-        {
-          // event propagation has been allowed intentionally to hide the dropdown on select
-          Array.from(
-            Array(suraList[selectedSuraNum - 1]?.ayah_count || 0).keys(),
-          ).map((index) => (
-            <div
-              key={index}
-              className="AyahSelect-Option"
-              onClick={() => onSelectAyah(index + 1)}
-            >
-              <span className="AyahSelect-Option-Num">{index + 1}</span>
-            </div>
-          ))
-        }
-      </div>
-    </div>
+    <NumberSelect
+      valueClassName={valueClassName}
+      selectedNumber={selectedAyahNum}
+      startNumber={1}
+      endNumber={ayahCount}
+      onSelectNumber={(number) => onSelectAyah(number)}
+    />
   );
 }
 
