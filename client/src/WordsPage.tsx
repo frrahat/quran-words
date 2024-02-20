4a61072f6188b81c3c0f77fd6432859707b288da
frrahat <fr.rahat@gmail.com>
2024-02-20 23:55:38 +0600

    support getting occurrences from frequency item

--- client/src/WordsPage.tsx
+++ client/src/WordsPage.tsx
@@ -1,4 +1,6 @@
+import { useRef } from "react";
 import Frequencies from "./components/Frequencies";
+import Occurrences from "./components/Occurrences";
 import { generateWordsPageLink, parseIntFromQuery, useQuery } from "./utils";
 import { useHistory } from "react-router";
 
@@ -8,22 +10,57 @@ function WordsPage() {
 
   const occurrencePage = parseIntFromQuery(query, "occurrence_page");
   const taraweehNight = parseIntFromQuery(query, "taraweeh_night");
-  const frequencyPage = parseIntFromQuery(query, "frequency_page");
+  const frequencyPage = parseIntFromQuery(query, "frequency_page", 1) as number;
+  const wordRoot = query.get("root");
+  const wordLemma = query.get("lemma");
+
+  const pageTopRef = useRef<HTMLDivElement>(null);
+
+  const paginatorLinkGenerator = (pageNum: number) =>
+    generateWordsPageLink({
+      occurrence_page: occurrencePage,
+      taraweeh_night: taraweehNight,
+      frequency_page: pageNum,
+      root: wordRoot,
+      lemma: wordLemma,
+    });
 
   return (
-    <Frequencies
-      taraweehNight={taraweehNight}
-      frequencyPage={frequencyPage || 1}
-      frequencyItemIndex={0}
-      paginatorLinkGenerator={(currentPage: number) =>
-        generateWordsPageLink({
-          occurrence_page: occurrencePage,
-          taraweeh_night: taraweehNight,
-          frequency_item_index: undefined,
-          frequency_page: currentPage,
-        })
-      }
-    />
+    <div className="WordsPage">
+      {(wordRoot || wordLemma) && (
+        <div className="WordsPage-LeftPanel" ref={pageTopRef}>
+          <Occurrences
+            wordRoot={wordRoot}
+            wordLemma={wordLemma}
+            occurrencePage={occurrencePage || 1}
+            pageTopRef={pageTopRef}
+            paginatorLinkGenerator={paginatorLinkGenerator}
+            taraweehNight={taraweehNight}
+          />
+        </div>
+      )}
+      <div className="WordsPage-RightPanel">
+        <Frequencies
+          taraweehNight={taraweehNight}
+          frequencyPage={frequencyPage || 1}
+          onSelectFrequencyItem={(
+            root: string | undefined,
+            lemma: string | undefined,
+          ) => {
+            const pageLink = generateWordsPageLink({
+              occurrence_page: occurrencePage,
+              taraweeh_night: taraweehNight,
+              frequency_page: frequencyPage,
+              root: root,
+              lemma: lemma,
+            });
+
+            history.push(pageLink);
+          }}
+          paginatorLinkGenerator={paginatorLinkGenerator}
+        />
+      </div>
+    </div>
   );
 }
 
