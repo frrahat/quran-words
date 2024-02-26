7d89b439cf8af01582f368deb7cf5f2767642390
frrahat <fr.rahat@gmail.com>
2024-02-26 16:39:37 +0600

    format code with black and isort

--- server/app/utils.py
+++ server/app/utils.py
@@ -3,41 +3,36 @@ from typing import Dict, Optional
 from fastapi import Request
 
 
-def get_next_prev_paginations(
-        current_offset: int,
-        current_pagesize: int,
-        count: int):
+def get_next_prev_paginations(current_offset: int, current_pagesize: int, count: int):
 
     prev_pagination = {}
     next_pagination = {}
 
     if current_offset > 0:
-        prev_pagination['offset'] = max(0, current_offset - current_pagesize)
-        prev_pagination['pagesize'] = min(current_pagesize, current_offset)
+        prev_pagination["offset"] = max(0, current_offset - current_pagesize)
+        prev_pagination["pagesize"] = min(current_pagesize, current_offset)
 
     next_offset = current_offset + current_pagesize
 
     if next_offset < count:
-        next_pagination['offset'] = next_offset
-        next_pagination['pagesize'] = min(
-            current_pagesize, count - next_offset + 1)
+        next_pagination["offset"] = next_offset
+        next_pagination["pagesize"] = min(current_pagesize, count - next_offset + 1)
 
     return prev_pagination, next_pagination
 
 
 def get_pagination_response(
-        request: Request,
-        count: int,
-        additional_query_string: str = '',
-        limit: int = 10) -> Dict[str, Optional[str]]:
+    request: Request, count: int, additional_query_string: str = "", limit: int = 10
+) -> Dict[str, Optional[str]]:
 
-    current_offset = int(request.query_params.get('offset', 0))
-    current_pagesize = int(request.query_params.get('pagesize', limit))
+    current_offset = int(request.query_params.get("offset", 0))
+    current_pagesize = int(request.query_params.get("pagesize", limit))
 
     prev_pagination, next_pagination = get_next_prev_paginations(
-        current_offset, current_pagesize, count)
+        current_offset, current_pagesize, count
+    )
 
-    additional_query_params = additional_query_string.split('&')
+    additional_query_params = additional_query_string.split("&")
     non_none_query_params = list(
         filter(
             lambda param: len(param) > 0 and not param.endswith("=None"),
@@ -51,10 +46,16 @@ def get_pagination_response(
     url = request.url.path
 
     return {
-        'previous': f"{url}?offset={prev_pagination['offset']}"
-        f"&pagesize={prev_pagination['pagesize']}{additional_query_string}"
-        if prev_pagination else None,
-        'next': f"{url}?offset={next_pagination['offset']}"
-        f"&pagesize={next_pagination['pagesize']}{additional_query_string}"
-        if next_pagination else None,
+        "previous": (
+            f"{url}?offset={prev_pagination['offset']}"
+            f"&pagesize={prev_pagination['pagesize']}{additional_query_string}"
+            if prev_pagination
+            else None
+        ),
+        "next": (
+            f"{url}?offset={next_pagination['offset']}"
+            f"&pagesize={next_pagination['pagesize']}{additional_query_string}"
+            if next_pagination
+            else None
+        ),
     }
