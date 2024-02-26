0b05d503110f30c3c7ea779e5ca16f6b82327877
frrahat <fr.rahat@gmail.com>
2024-02-26 16:38:12 +0600

    use session from SessionMakerMixin to execute queries

--- server/app/tests/test_db_corpus.py
+++ server/app/tests/test_db_corpus.py
@@ -1,17 +1,19 @@
-from app.db_corpus import db_corpus, Corpus, POS_FULL_FORMS_AND_COLORS
+from app.db.db_corpus import POS_FULL_FORMS_AND_COLORS, Corpus
 
 
 def test_all_pos_forms_are_listed():
     pos_in_db = set()
-    for position in range(1, 6):
-        pos_in_db = pos_in_db.union(
-            {
-                getattr(c, f"pos{position}")
-                for c in db_corpus.session.query(getattr(Corpus, f"pos{position}"))
-                .distinct()
-                .all()
-            }
-        )
+
+    with Corpus.get_session() as session:
+        for position in range(1, 6):
+            pos_in_db = pos_in_db.union(
+                {
+                    getattr(c, f"pos{position}")
+                    for c in session.query(getattr(Corpus, f"pos{position}"))
+                    .distinct()
+                    .all()
+                }
+            )
 
     pos_in_db.remove(None)
 
